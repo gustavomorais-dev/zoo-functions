584d4145254ba23355b8e6445ac13fb03d873f6c
gustavomorais <gustavomorais.dev@gmail.com>
2023-02-03 02:35:51 -0300

    Completa o requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
+const getSpeciesByIds = (...args) => {
   // seu código aqui
+  let array = [];
+  args.forEach((arg) => {
+    data.species.forEach((specie) => {
+      if (specie.id == arg) array.push(specie); 
+    })
+  });
+  return array;
 };
 
 module.exports = getSpeciesByIds;
