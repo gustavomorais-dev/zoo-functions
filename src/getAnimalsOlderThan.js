e24018c7bd5ef909681063116b6b59473686cdb5
gustavomorais <gustavomorais.dev@gmail.com>
2023-02-03 02:57:52 -0300

    completa o requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,8 @@ const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
   // seu código aqui
+  const getSpecie = data.species.find((specie) => specie.name === animal);
+  return getSpecie.residents.every((resident) => resident.age >= age);
 };
 
 module.exports = getAnimalsOlderThan;
