b4add8b90f3ee5da266ddb90b355f639f9e671df
gustavomorais <gustavomorais.dev@gmail.com>
2023-02-03 03:43:11 -0300

    completa o requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (typeof employeeName === 'undefined') return {};
+  return data.employees.find((e) => e.firstName === employeeName || e.lastName === employeeName);
 };
 
 module.exports = getEmployeeByName;
