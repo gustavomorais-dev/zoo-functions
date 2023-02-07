5a544d61444f7204a47fff231c856b383dbf7126
gustavomorais <gustavomorais.dev@gmail.com>
2023-02-07 05:14:56 -0300

    completa o requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const filterBySpecies = (species) => (data.species.filter((s) => s.name === species)[0]);
+
 const countAnimals = (animal) => {
-  // seu código aqui
+  // Adquire as keys passadas como parâmetro
+  const keys = typeof animal !== 'undefined' ? Object.keys(animal) : [];
+  // Declara o retorno
+  let result;
+  // Formata o retorno
+  if (keys.includes('sex')) {
+    const filteredBySpecies = filterBySpecies(animal.species);
+    const filteredBySex = filteredBySpecies.residents.filter((r) => r.sex === animal.sex);
+    result = filteredBySex.length;
+  } else if (keys.includes('species')) {
+    const filteredBySpecies = filterBySpecies(animal.species);
+    result = filteredBySpecies.residents.length;
+  } else {
+    result = {};
+    data.species.forEach((specie) => { result[specie.name] = specie.residents.length; });
+    console.log('wtf');
+  }
+  return result;
 };
 
 module.exports = countAnimals;
