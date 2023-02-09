f38b273da1ed5c5f75b5e7a765c24c170a5a288c
gustavomorais <gustavomorais.dev@gmail.com>
2023-02-09 13:58:17 -0300

    completa o requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const getFirstAnimal = (id) => {
+  const getEmployee = data.employees.filter((employee) => employee.id === id);
+  const animalId = getEmployee[0].responsibleFor[0];
+  return data.species.filter((specie) => specie.id === animalId);
+};
+
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const specie = getFirstAnimal(id);
+  const sorted = specie[0].residents.sort((a, b) => b.age - a.age);
+  return [sorted[0].name, sorted[0].sex, sorted[0].age];
 };
 
 module.exports = getOldestFromFirstSpecies;
